ac97b5642cfcf3b0d3b64bc90d3166c527d8fb0e
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2019-12-18 19:35:01 -0800

    built react app and file structure

---
+++ salty-hacker-trolls/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     setup users/login<br />
+     setup private routes<br />
+     setup data .get from axios call<br />
+     setup rendering of data<br />
+     setup .put for adding favs to user 
+    </div>
+  );
+}
+
+export default App;
+
