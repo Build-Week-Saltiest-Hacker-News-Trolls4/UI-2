af97310a16351424bfa8ca238f5daf2199ff124d
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2019-12-18 20:07:51 -0800

    cleaned up file tree

--- salty-hacker-trolls/src/App.js
+++ salty-hacker-trolls/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// // setup users/login<br />
+// setup private routes<br />
+// setup data .get from axios call<br />
+// setup rendering of data<br />
+// setup .put for adding favs to user
+
 function App() {
   return (
     <div className="App">
-     setup users/login<br />
-     setup private routes<br />
-     setup data .get from axios call<br />
-     setup rendering of data<br />
-     setup .put for adding favs to user 
+      
     </div>
   );
 }
