ada7f0a6dd3d9b4cca76cc5b0c049cef63aedc13
Sable <sablechilds@yahoo.com>
2020-01-13 20:56:05 -0500

    new-pr

--- salty-hacker-trolls/src/App.js
+++ salty-hacker-trolls/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import PrivateRoute from './utils/privateRoute';
+import SignUp from '/components/SignUp/SignUp';
 function App() {
   return (
+    <Router>
     <div className="App">
+    <Route exact path='/signup' component={SignUp} />
      setup users/login<br />
      setup private routes<br />
      setup data .get from axios call<br />
      setup rendering of data<br />
      setup .put for adding favs to user 
     </div>
+    </Router>
   );
 }
 
---
+++ salty-hacker-trolls/src/components/SignUp.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+
+import './SignUp.css';
+
+class SignUp extends Component {
+  state = {
+    email: '',
+    password: '',
+    username: ''
+  };
+
+  return (
+    <div> 
+    <form>
+    </form>
+    </div>
+  )
+  
+}
+
+//
